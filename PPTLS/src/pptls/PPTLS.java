e5517407880200db999344051d4c514345533862
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-03-25 21:38:32 -0500

    Add files via upload

---
+++ PPTLS/src/pptls/PPTLS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pptls;
+
+/**
+ *
+ * @author david
+ */
+public class PPTLS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmmenu frmmenu1 = new frmmenu();
+        frmmenu1.setVisible(true);
+        frmmenu1.setSize(500,500);
+        frmmenu1.setResizable(false);
+        frmmenu1.setLocationRelativeTo(null);
+    }
+    
+}
